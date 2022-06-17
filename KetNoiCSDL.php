315f7ea6ba739f315c6faf5f295ff633205c9902
PhamDinh <phamdinh2001pt@gmail.com>
2022-06-17 18:58:39 +0700

    Fix loi tieng Viet

--- KetNoiCSDL.php
+++ KetNoiCSDL.php
@@ -3,4 +3,6 @@ $conn = mysqli_connect("localhost","root","123456","quanlykhohang");
 if(!$conn){
 	echo("Không thể kết nối tới cơ sở dữ liệu");
 }
+header("Content-type: text/html; charset=utf-8");
+mysqli_set_charset($conn, 'UTF8');
 ?>
